1e24feb4b40286a0e05c7815476bdfbdca3976ce
haruna <haruharu_1155@gmail.com>
2020-08-24 02:37:37 +0000

    add column

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -15,7 +15,7 @@
                 
                 <div class="form-group">
                     {!! Form::label("status","ステータス:") !!}
-                    {!! Form::text("status",null,["class"=>form-control"] !!}
+                    {!! Form::text("status",null,["class"=>"form-control"]) !!}
                 </div>
 
                 {!! Form::submit('更新', ['class' => 'btn btn-primary']) !!}
