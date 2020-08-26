8f8c8f8f50e60aecc3c5bb383e9ab860158e57b8
haruna <haruharu_1155@gmail.com>
2020-08-26 01:41:43 +0000

    login

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,31 @@
+<header class="md-4">
+    <nav class="navbar navbar-expand navbar-light bg-info">
+        <a class="navbar-brand" href="/">Tasklist</a>
+        
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse" id="nav-bar">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav">
+                @if (Auth::check())
+                    <li class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</a>
+                        <ul class="dropdown-menu dropdown-menu-right">
+                            {{-- 新規タスク追加 --}}
+                            <li class="doropdown-item"><a href="#">{!! link_to_route('tasks.create', '新規タスクを追加', [], ['class' => 'nav-link']) !!}</a></li>
+                            {{-- ログアウト --}}
+                            <li class="doropdown-item">{!! link_to_route('logout.get', 'Logout') !!}</li>
+                        </ul>
+                    </li>
+                @else
+                    {{-- ユーザ登録ページへのリンク --}}
+                    <li class="nav-item">{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+                    {{-- ログインページへのリンク --}}
+                    <li class="nav-item">{!! link_to_route('login', 'Login', [], ['class' => 'nav-link']) !!}</li>
+                @endif
+            </ul>    
+        </div>      
+    </nav>
+</header>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,22 +8,8 @@
     </head>
     
     <body>
-        <header class="md-4">
-            <nav class="navbar navbar-expand navbar-light bg-info">
-            <a class="navbar-brand" href="/">Tasklist</a>
-            
-            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            
-            <div class="collapse navbar-collapse" id="nav-bar">
-                    <ul class="navbar-nav mr-auto"></ul>
-                    <ul class="navbar-nav">
-                        <li class="nav-item">{!! link_to_route('tasks.create', '新規タスクを追加', [], ['class' => 'nav-link']) !!}</li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
+        {{ --ナビバージョンバー --}
+        @include("commons.navbar")
         
         <div class="container">
             {{--エラーメッセージ --}}
